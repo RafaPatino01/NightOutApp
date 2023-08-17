8c67b67a4daff28ddf41b6f1ae8a0da0019588cd
Rafael Patino Goji <RafaPatino01@outlook.com>
2023-08-17 11:25:15 -0600

    navigation done

---
+++ front-end/NightOut/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Screen1 from './screens/Screen1';
+import Screen2 from './screens/Screen2';
+import Screen3 from './screens/Screen3';
+import Screen4 from './screens/Screen4';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function Home() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Screen1') {
+            iconName = 'home';
+          } else if (route.name === 'Screen2') {
+            iconName = 'account';
+          } else if (route.name === 'Screen3') {
+            iconName = 'cog'; // Ícono de configuración
+          }
+          
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Screen1" component={Screen1} />
+      <Tab.Screen name="Screen2" component={Screen2} />
+      <Tab.Screen name="Screen3" component={Screen3} />
+    </Tab.Navigator>
+  );
+}
+
+function App() {
+
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Screen4" component={Screen4} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
+export default App;
---
+++ front-end/NightOut/screens/Screen3.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const Screen3 = () => {
+  const navigation = useNavigation();
+
+  const data = Array.from({ length: 20 }, (_, index) => ({
+    id: `${index + 1}`,
+    title: `Item ${index + 1}`,
+  }));
+
+  const handleItemPress = (item) => {
+	navigation.navigate('Screen4');
+  };
+  
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleItemPress(item)}>
+      <View style={styles.item}>
+        <Text>{item.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.flatlist}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  flatlist: {
+    width: '100%',
+  },
+  item: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+});
+
+export default Screen3;
